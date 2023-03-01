4455fd053a0b7fd0bef613943be6162a80b5953c
Limezy <arnaudetpia@de-ruffray.fr>
2023-03-02 01:16:58 +0700

    Update _common.sh

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -14,7 +14,7 @@ pkg_dependencies=""
 function build_buckets {
 	pushd "$final_path"
 		chown -R $app:$app $final_path
-		sudo -u $app env "PATH=$final_path/nim-installation/bin:$(sudo -u $app sh -c 'echo $PATH')" nimble install -y --depsOnly --verbose
+		sudo -u $app env "PATH=$final_path/nim-installation/bin:$(sudo -u $app sh -c 'echo $PATH')" nimble install -y --depsOnly --verbose 2>&1
 		sudo -u $app env "PATH=$final_path/nim-installation/bin:$(sudo -u $app sh -c 'echo $PATH')" nim c -o:brelay -d:relaysingleusermode -d:release src/brelay 2>&1
 		chown -R root:root $final_path
 	popd
